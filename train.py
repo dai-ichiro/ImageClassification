e6569e450177eedb8874f6fa2073afa8905eeeb0
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-03-06 14:00:14 +0900

    create autommpredictor branch

--- train.py
+++ train.py
@@ -1,19 +1,22 @@
+import os
 import pandas as pd
-from autogluon.vision import ImageDataset
-from autogluon.vision import ImagePredictor
-from autogluon .core.space import Categorical
+from autogluon.text.automm import AutoMMPredictor
 
 train_df = pd.read_pickle('train_df.pkl')
 
-train_dataset = ImageDataset(train_df)
+train_df['image'] = train_df['image'].apply(lambda x: os.path.abspath(x))
 
-predictor = ImagePredictor()
-model = Categorical('resnet101_v1d')
-hyperparameters = {'model':model, 'batch_size':32, 'epochs': 5}
-hyperparameter_tune_kwargs={'num_trials': 2}
+
+predictor = AutoMMPredictor(label='label')
 predictor.fit(
-    train_dataset, 
-    hyperparameters = hyperparameters,
-    hyperparameter_tune_kwargs = hyperparameter_tune_kwargs
-    )
-predictor.save('predictor.ag')
+    train_data=train_df,
+    hyperparameters={
+        "model.names": ["timm_image"],
+        "model.timm_image.checkpoint_name": "swin_tiny_patch4_window7_224",
+        "env.num_gpus": 1,
+    }
+)
+
+predictor.save('my_saved_dir')
+
+
