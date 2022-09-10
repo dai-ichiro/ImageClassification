de34be2945ff6af862db54164e5b8c088e36b17f
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-09-10 13:05:03 +0900

    Add files via upload

--- train.py
+++ train.py
@@ -1,18 +1,12 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 import pandas as pd
-from autogluon.text.automm import AutoMMPredictor
+from autogluon.multimodal import MultiModalPredictor
 
 train_df = pd.read_pickle('train_df.pkl')
 
-predictor = AutoMMPredictor(label='label')
-predictor.fit(
-    train_data=train_df,
-    hyperparameters={
-        "model.names": ["timm_image"],
-        "model.timm_image.checkpoint_name": "swin_tiny_patch4_window7_224",
-        "env.num_gpus": 1,
-    }
-)
+predictor = MultiModalPredictor(label="label")
+predictor.fit(train_data = train_df)
 
 predictor.save('my_saved_dir')
-
-
